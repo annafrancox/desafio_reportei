0f4cbdcf4834c4cfa47861d57e729e1a188d7828
annafrancox <annaleticiafranco31@gmail.com>
2022-01-23 22:36:46 -0300

    tentativa de hospedagem com banco de dado corrigido

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'access' => "Usuário Comum",
+            'name' => "admin",
+            'email' => "admin@admin",
+            'access' => "administrador",
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('123456789'), // password
             'remember_token' => Str::random(10),
         ];
     }
